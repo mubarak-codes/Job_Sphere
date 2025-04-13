4731e0684b9faf4542300a147d58879218b2e4f6
mubarak-codes <mubarakcodes@gmail.com>
2025-04-13 21:17:02 +0100

    adf favicon and logo

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -5,17 +5,17 @@ import {Link} from 'react-router-dom'
 const Navbar = ()=>{
   return(
     <section>
-      <nav className="navbar navbar-expand bg-danger">
+      <nav className="navbar navbar-expand bg-danger py-sm-5 ">
         <div className="container-fluid mx-3"> 
           <div className="navbar-brand ">
-           <img src={logo} className="me-3 logo" />
-           <span className="text-white fw-bold d-none d-sm-inline-block ff-rubik">JOB SPHERE</span>
+           <img src="img/Logo.png" className="me-3 logo w-60 w-10-sm " />
+           <span className="text-white fw-bold d-none d-sm-inline-block ff-rubik mfs-7-sm">JOB SPHERE</span>
           </div>
           
           <div className="navbar-nav">
-           <Link to="/" className="nav-item text-white">Home</Link>
-           <Link to="/jobs" className="nav-item text-white">Jobs</Link>
-           <Link to="/add-job" className="nav-item text-white">Add Job</Link> 
+           <Link to="/" className="nav-item text-white mfs-4-sm">Home</Link>
+           <Link to="/jobs" className="nav-item text-white mfs-4-sm">Jobs</Link>
+           <Link to="/add-job" className="nav-item text-white mfs-4-sm">Add Job</Link> 
           </div>
         </div>
       </nav>
